c88ab4f94b53cd630581148c60dbc38417884934
sachu0dev <sushil.dev.in@gmail.com>
2024-04-08 18:21:04 +0530

    api end points

--- frontend/js/NotesAPI.js
+++ frontend/js/NotesAPI.js
@@ -3,7 +3,7 @@ export default class NotesAPI {
     // Assuming you have the JWT token stored in localStorage as 'token'
 const token = localStorage.getItem('token');
 
-   const response = await fetch('http://localhost:3000/notes', {
+   const response = await fetch('https://notesapp-five-steel.vercel.app/notes', {
       method: 'GET',
       headers: {
         'Authorization': token
@@ -16,7 +16,7 @@ const token = localStorage.getItem('token');
 
   static async saveNote(noteToSave) {
     const token = localStorage.getItem('token');
-    const response = await fetch('http://localhost:3000/notes', {
+    const response = await fetch('https://notesapp-five-steel.vercel.app/notes', {
       method: 'POST',
       headers: {
         'Authorization': token,
@@ -29,7 +29,7 @@ const token = localStorage.getItem('token');
 
   static async deleteNote(id) {
     const token = localStorage.getItem('token');
-    const response = await fetch('http://localhost:3000/notes', {
+    const response = await fetch('https://notesapp-five-steel.vercel.app/notes', {
       method: 'DELETE',
       headers: {
         'Authorization': token,
